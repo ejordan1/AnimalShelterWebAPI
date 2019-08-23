ada1fb90704befceab5356f99e3589d4036cfc8b
Student <example@example.com>
2019-08-23 15:46:10 -0700

    add post and put. I finally figured out why postman wasn't working; it was a setting that blocked it and wasted a lot of time.

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -28,5 +28,21 @@ namespace AnimalShelter.Controllers
             _db.Animals.Add(animal);
             _db.SaveChanges();
         }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Animal animal)
+        {
+            animal.AnimalId = id;
+            _db.Entry(animal).State = EntityState.Modified;
+            _db.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var animalToDelete =_db.Animals.FirstOrDefault(animal => animal.AnimalId == id);
+            _db.Animals.Remove(animalToDelete);
+            _db.SaveChanges();
+        }
     }
 }
